194223c114db9e977ea94cfdbdcb34f4d498617b
Alexey <46936937+AlexDeKarlo@users.noreply.github.com>
2024-11-17 02:52:05 +0400

    fix canned-jellynut icon

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -11,7 +11,7 @@ data:extend({
 {
   type = "item",
   name = "canned-jellynut",
-  icon = "__canning-and-freezing__/graphics/icons/canned-yumako.png", 
+  icon = "__canning-and-freezing__/graphics/icons/canned-jellynut.png", 
   icon_size = 64,
   stack_size = 10,
   spoil_ticks = 180 * minute,
